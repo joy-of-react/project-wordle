cb557ff66cce5c849933c1358e81e74314a03c5f
Joshua Comeau <joshwcomeau@gmail.com>
2022-04-26 17:47:44 -0400

    Exercise 3 solution

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { range } from '../../utils';
+
+function Guess({ value }) {
+  return (
+    <p className="guess">
+      {range(5).map((num) => (
+        <span key={num} className="cell">
+          {value ? value[num] : undefined}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+
+import Guess from '../Guess';
+
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p key={index} className="guess">
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
