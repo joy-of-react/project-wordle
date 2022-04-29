f3683b1c8544ff874ad220903afe227c80243ec4
Joshua Comeau <joshwcomeau@gmail.com>
2022-04-29 14:16:58 -0400

    Exercise 4 solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -20,7 +20,7 @@ function Game() {
 
   return (
     <>
-      <GuessResults guesses={guesses} />
+      <GuessResults guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -5,11 +5,11 @@ import { range } from '../../utils';
 
 import Guess from '../Guess';
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guesses[num]} />
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   );
