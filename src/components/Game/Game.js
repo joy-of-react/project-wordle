c5fa53d2a408bb884c674b7f578b26ea9c5f8d68
Joshua Comeau <joshwcomeau@gmail.com>
2022-04-30 15:30:24 -0400

    Exercise 1 solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,13 +3,19 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
+import GuessInput from '../GuessInput';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return (
+    <>
+      <GuessInput />
+    </>
+  );
 }
 
 export default Game;
