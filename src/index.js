ec8edaf1bf0c0f68c9ed553f3dfbfa406ea5d564
Joshua Comeau <joshwcomeau@gmail.com>
2022-06-19 15:53:03 -0400

    Update to createRoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
 import './reset.css';
 import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.querySelector('#root'));
+root.render(<App />);
