e3da18dd0ad3737ba9440d5dc54eaf393d57e3ce
Victor Caruso <vic.caruso@gmail.com>
2022-10-26 16:17:42 -0700

    add submit handler to form

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
   return (
-    <form>
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter Guess:</label>
       <input
         id="guess-input"
