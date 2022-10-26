0661b12328b98dd3c7bcea36e82a7206879476d4
Victor Caruso <vic.caruso@gmail.com>
2022-10-26 16:09:11 -0700

    set form in GuessInput

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function GuessInput() {
-  return <div />;
+  const [guess, setGuess] = React.useState("");
+  return (
+    <form>
+      <label htmlFor="guess-input">Enter Guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value)}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
