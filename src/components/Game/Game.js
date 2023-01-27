fd7a2196ca0e2627ed834d74a212ba61e5b84a95
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2023-01-27 17:41:29 -0300

    exercise 1

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,17 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+
+  function handleGuessSent(value) {
+    console.log({ guess: value });
+    setGuess(value);
+  }
+  return (
+    <>
+      <GuessInput handleGuessSent={handleGuessSent} />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function GuessInput({ handleGuessSent }) {
+  const [guessValue, setGuessValue] = React.useState("");
+
+  function onChangeGuess(event) {
+    const { value } = event.target;
+
+    setGuessValue(value.trim().toUpperCase());
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (guessValue.length !== 5) return;
+
+    handleGuessSent(guessValue);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="guess">Enter guess:</label>
+      <input
+        id="guess"
+        type="text"
+        value={guessValue}
+        onChange={onChangeGuess}
+        maxLength={5}
+        minLength={5}
+        required
+      />
+      <button>Guess</button>
+    </form>
+  );
+}
+
+export default GuessInput;
