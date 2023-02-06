63c008a824dd147a16e80836853a279c6e826c8e
Sean Dashner <seandashner@gmail.com>
2023-02-06 10:26:47 -0500

    exercise one solutin

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from '../../utils'
+import { WORDS } from '../../data'
+import GuessInput from '../GuessInput/GuessInput'
+
+const DEFAULT_GUESS = ''
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({ answer })
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = useState(DEFAULT_GUESS)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.info({ guess })
+    setGuess(DEFAULT_GUESS)
+  }
+
+  const handleChange = (e) => setGuess(e.target.value.toUpperCase())
+
+  return (
+    <GuessInput
+      handleSubmit={handleSubmit}
+      handleChange={handleChange}
+      guess={guess}
+    />
+  )
 }
 
-export default Game;
+export default Game
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+function GuessInput({handleSubmit, handleChange, guess}) {
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        pattern="[A-Z]{5,5}"
+        value={guess}
+        onChange={handleChange}
+      />
+    </form>
+  )
+}
+
+export default GuessInput
