68dc3da1f7b07377b73d98e018ca3dd6cdb0bd44
Dylan Oliver <d@doliver.net>
2023-02-07 16:49:29 -0800

    Update `checkGuess` matching logic

--- src/game-helpers.js
+++ src/game-helpers.js
@@ -1,3 +1,5 @@
+const SOLVED_CHAR = '/';
+
 export function checkGuess(guess, answer) {
   if (!guess) {
     return null;
@@ -6,20 +8,40 @@ export function checkGuess(guess, answer) {
   const guessChars = guess.toUpperCase().split('');
   const answerChars = answer.split('');
 
-  return guessChars.map((guessChar, index) => {
-    const answerChar = answerChars[index];
+  const result = [];
 
-    let status;
-    if (guessChar === answerChar) {
-      status = 'correct';
-    } else if (answerChars.includes(guessChar)) {
-      status = 'misplaced';
-    } else {
-      status = 'incorrect';
+  // Check for correct characters first, and remove matches from the answer
+  for (let i = 0; i < guessChars.length; i++) {
+    if (guessChars[i] === answerChars[i]) {
+      result[i] = {
+        letter: guessChars[i],
+        status: 'correct',
+      };
+      answerChars[i] = SOLVED_CHAR;
+      guessChars[i] = SOLVED_CHAR;
     }
-    return {
-      letter: guessChar,
+  }
+
+  // Check for misplaced letters from the remaining characters
+  for (let i = 0; i < guessChars.length; i++) {
+    if (guessChars[i] === SOLVED_CHAR) {
+      continue;
+    }
+
+    let status = 'incorrect';
+    const misplacedIndex = answerChars.findIndex(
+      (char) => char === guessChars[i]
+    );
+    if (misplacedIndex >= 0) {
+      status = 'misplaced';
+      answerChars[misplacedIndex] = SOLVED_CHAR;
+    }
+
+    result[i] = {
+      letter: guessChars[i],
       status,
     };
-  });
+  }
+
+  return result;
 }
