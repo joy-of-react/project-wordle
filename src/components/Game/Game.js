39f2b34f98cd285953382a26568bd4af0f8af341
Joshua Comeau <joshwcomeau@gmail.com>
2023-02-23 13:11:23 -0500

    Lock to 'works'

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
 import { checkGuess } from '../../game-helpers';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
@@ -10,9 +8,7 @@ import GuessResults from '../GuessResults';
 import WonBanner from '../WonBanner';
 import LostBanner from '../LostBanner';
 
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
+const answer = 'WORKS';
 console.info({ answer });
 
 function Game() {
