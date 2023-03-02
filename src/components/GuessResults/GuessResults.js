26198aa71b434b503da667ca9cddf4c38d122532
devinegger <itsme@devinegger.cc>
2023-03-02 12:08:34 -0500

    testing push from mbp

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -6,15 +6,18 @@ import Guess from "../Guess";
 
 function GuessResults({ guessList }) {
   return (
-    <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((num) =>
-        guessList[num] !== undefined ? (
-          <Guess guess={guessList[num]} index={num} />
-        ) : (
-          <Guess index={num} />
-        )
-      )}
-    </div>
+    <>
+      <div className="guess-results">
+        {range(NUM_OF_GUESSES_ALLOWED).map((num) =>
+          guessList[num] !== undefined ? (
+            <Guess guess={guessList[num]} index={num} />
+          ) : (
+            <Guess index={num} />
+          )
+        )}
+      </div>
+      <p>something</p>
+    </>
   );
 }
 
