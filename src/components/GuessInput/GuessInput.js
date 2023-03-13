9a2345016ec2b68a24faa1349e0f017b972321f9
Nate Hunter <nhunter@indemand.com>
2023-03-13 11:07:59 -0400

    added validation to `input` element

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -18,7 +18,14 @@ const GuessInput = () => {
   return (
     <form className='guess-input-wrapper' onSubmit={handleSubmit}>
       <label htmlFor='guess-input'>Enter guess:</label>
-      <input type='text' name='guess-input' id='guess-input' value={guess} onChange={handleGuess} />
+      <input
+        type='text'
+        name='guess-input'
+        id='guess-input'
+        value={guess}
+        onChange={handleGuess}
+        pattern='[a-zA-Z]{5}'
+      />
     </form>
   );
 };
