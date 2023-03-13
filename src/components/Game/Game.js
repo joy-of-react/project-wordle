78f3522c5b158c9087e077d992b836f6670d3322
Nate Hunter <nhunter@indemand.com>
2023-03-13 10:43:49 -0400

    created new component `GuessInput` + imported to `Game` component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,13 +3,22 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
+import GuessInput from './../GuessInput';
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function Game() {
-  return <>Put a game here!</>;
+
+// Using arrow functions as that is what I tend to use personally / professionally.
+// Also, using as a way to track files I touch - maybe helpful, maybe not
+const Game = () => {
+  return (
+    <>
+      <GuessInput />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuessInput = () => {
+  return (
+    <>
+      <h3>GuessInput</h3>
+    </>
+  );
+}
+
+export default GuessInput;
