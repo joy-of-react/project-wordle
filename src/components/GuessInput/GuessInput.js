1b8441814b2fd2cc1528c7764fc98a19953cbc2d
Nate Hunter <nhunter@indemand.com>
2023-03-13 10:57:07 -0400

    added `.prettierrc` file + ran on `GuessInput`

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const GuessInput = () => {
   const [guess, setGuess] = useState('');
 
   const handleGuess = (e) => {
-    const {value} = e.target;
-    setGuess(value.toUpperCase())
-  }
+    const { value } = e.target;
+    setGuess(value.toUpperCase());
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log({guess})
-    setGuess('')
-  }
+    console.log({ guess });
+    setGuess('');
+  };
 
   return (
     <form className='guess-input-wrapper' onSubmit={handleSubmit}>
       <label htmlFor='guess-input'>Enter guess:</label>
-      <input type="text" name="guess-input" id="guess-input" value={guess} onChange={handleGuess} />
+      <input type='text' name='guess-input' id='guess-input' value={guess} onChange={handleGuess} />
     </form>
   );
-}
+};
 
 export default GuessInput;
