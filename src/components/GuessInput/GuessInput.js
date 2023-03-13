b1bc5c5e5ac4cbed7a21a2994a82ce70e7f355aa
Nate Hunter <nhunter@indemand.com>
2023-03-13 10:55:14 -0400

    set up 's controlled form + handler functions

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GuessInput = () => {
+  const [guess, setGuess] = useState('');
+
+  const handleGuess = (e) => {
+    const {value} = e.target;
+    setGuess(value.toUpperCase())
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({guess})
+    setGuess('')
+  }
+
   return (
-    <form className='guess-input-wrapper'>
+    <form className='guess-input-wrapper' onSubmit={handleSubmit}>
       <label htmlFor='guess-input'>Enter guess:</label>
-      <input type="text" name="guess-input" id="guess-input" />
+      <input type="text" name="guess-input" id="guess-input" value={guess} onChange={handleGuess} />
     </form>
   );
 }
