cbeba074054b1aab77b43c17873ca5ac9f6cfb76
Alvi <contact.ivanmilosevic@gmail.com>
2023-03-19 14:42:04 +0100

    clean keyboard style

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -5,15 +5,15 @@ const checkLetterStatus = (guesses, letter) => {
   const allGuesses = guesses.flat().filter(d => d.letter === letter);
 
   if (allGuesses.some(d => d.status === 'correct')) {
-    return 'var(--color-success)';
+    return 'correct';
   }
 
   if (allGuesses.some(d => d.status === 'misplaced')) {
-    return 'var(--color-warning)';
+    return 'misplaced';
   }
 
   if (allGuesses.some(d => d.status === 'incorrect')) {
-    return 'var(--color-gray-300)';
+    return 'incorrect';
   }
 
   return '';
@@ -21,16 +21,17 @@ const checkLetterStatus = (guesses, letter) => {
 
 const QWERTY_LAYOUT = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
 function Keyboard({ guesses }) {
-  return <div style={{ paddingTop: '8px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '4px' }} >{QWERTY_LAYOUT.map(row => <div key={row} style={{ display: 'flex', gap: '4px' }}>
-    {row.split('').map(letter => <Letter key={letter}  letter={letter} guesses={guesses} />)}
-  </div>)}</div>
+  return <div className='keyboard'>{QWERTY_LAYOUT.map(row =>
+    <div key={row} className='keyboard-row'>
+      {row.split('').map(letter =>
+        <Letter key={letter} letter={letter} guesses={guesses} />)}
+    </div>
+  )}</div>
 }
 
 const Letter = ({ letter, guesses }) => {
-
   const status = checkLetterStatus(guesses, letter);
-
-  return <p style={{ color: status === '' ? 'black' : 'white', textAlign: 'center', width: 25, height: 25, border: '1px solid black', backgroundColor: status }}>{letter}</p>;
+  return <p className={`letter ${status}`}>{letter}</p>;
 }
 
 export default Keyboard;
