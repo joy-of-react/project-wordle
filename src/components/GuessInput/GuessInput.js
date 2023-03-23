883fc3fd57e986ccd452c467380f575494b6c16e
Ben Gordon <benharrisgordon@gmail.com>
2023-03-23 17:33:19 +0100

    Formatting

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,21 +1,32 @@
 import React from "react";
 
 function GuessInput() {
+  const [guess, setGuess] = React.useState("");
 
-  const [guess, setGuess] = React.useState('');
-
-
-  return(
-    <form className="guess-input-wrapper" onSubmit={event => {
-      event.preventDefault()
-      console.log(guess.toUpperCase())
-      setGuess('')
-    }}>
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(guess.toUpperCase());
+        setGuess("");
+      }}
+    >
       <label htmlFor="guess-input">Enter guess:</label>
-      <input pattern=".{5,}" required title="5 characters minimum" maxLength={5} id="guess-input" type="text" value={guess.toUpperCase()} onChange={event => {
-        setGuess(event.target.value)
-      }}/>
-    </form>);
+      <input
+        pattern=".{5,}"
+        required
+        title="5 characters minimum"
+        maxLength={5}
+        id="guess-input"
+        type="text"
+        value={guess.toUpperCase()}
+        onChange={(event) => {
+          setGuess(event.target.value);
+        }}
+      />
+    </form>
+  );
 }
 
 export default GuessInput;
