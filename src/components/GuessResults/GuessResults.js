61e43be2db3ab3a0a4a0700841bcb20b12400f26
Rick Pijnenburg <rpijnenburg92@gmail.com>
2023-05-15 17:36:19 +0200

    Implemented constants

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import Guess from '../Guess';
 
 function GuessResults({ validatedGuesses, answer }) {
-  const [numOfGuessesAllowed, setNumOfGuessesAllowed] = React.useState(5);
 
   return (
     <>
       <div className="guess-results">
-        {range(numOfGuessesAllowed).map((num) => (
+        {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
           <Guess key={num} value={validatedGuesses[num]} answer={answer} />
         ))}
       </div>
