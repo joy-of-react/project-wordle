72b9dbcd176b0c5144c581a56008d6c3562cf811
jack ashton <jackdashton@outlook.com>
2023-07-07 17:04:44 +0100

    file structure established

---
+++ word-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Word Game
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ word-clone/src/components/Game.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Game() {
+  return <div></div>;
+}
+
+export default Game;
