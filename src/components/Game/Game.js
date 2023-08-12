c4b6ae8657a92ff57f15ade0b43b1c93ad48e852
Mark Teekman <markteekman@users.noreply.github.com>
2023-08-12 13:35:51 +0200

    Add GuessInput to Game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GuessInput from '../GuessInput/GuessInput';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -9,7 +10,11 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return (
+    <>
+      <GuessInput />
+    </>
+  );
 }
 
 export default Game;
