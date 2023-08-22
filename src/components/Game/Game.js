e317a83367e1c539b278c10bc533228fc6f726ff
patricia pineda <patriciaquintanilla88@gmail.com>
2023-08-21 19:50:15 -0700

    exc -4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+
 import GuessedInput from "../GuessedInput";
 import Guesses from "../Guesses";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+// console.info({ answer });
 
 function Game() {
   // list of guesses in state
@@ -21,7 +22,7 @@ function Game() {
 
   return (
     <>
-      <Guesses guesses={guesses} />
+      <Guesses guesses={guesses} answer={answer} />
       <GuessedInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
+
+function Cell({ letter, status, index }) {
+  return (
+    <div className={`${status} cell`} key={index}>
+      {letter}
+    </div>
+  );
+}
 
 function Guess({ guess, answer }) {
+  const result = checkGuess(guess, answer);
   return (
     <div className="guess">
       {range(5).map((index) => {
-        return <div className="cell" key={index}></div>;
+        return (
+          <Cell
+            letter={result ? result[index].letter : undefined}
+            status={result ? result[index].status : undefined}
+            index={index}
+            key={index}
+          />
+        );
       })}
     </div>
   );
