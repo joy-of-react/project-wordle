c2524a67c5b2e87f21ea750ffb4516aae7c46dbe
fpigeonjr <frank.pigeonjr@gmail.com>
2023-08-21 21:36:50 -0500

    attempt at exercise one

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
-import Game from '../Game';
-import Header from '../Header';
+import Game from "../Game"
+import Header from "../Header"
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-
       <div className="game-wrapper">
         <Game />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
