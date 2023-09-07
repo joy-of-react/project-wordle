e61d84d74a4082358c2fd8a8f01a8191dc5bd18f
wihas <wihas@smile.fr>
2023-09-07 09:51:51 +0200

    Added missing constants and css

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,6 @@
 export const NUM_OF_GUESSES_ALLOWED = 6;
+export const CURRENT_LANGUAGE = "EN";
+export const KEYBOARD_LAYOUT = {
+  EN: "qwertyuiopasdfghjklzxcvbnm",
+  FR: "azertyuiopqsdfghjklmwxcvbn",
+};
