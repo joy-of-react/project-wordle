2ebbfe99eeaa3067118f5c363cae9f8fd2677105
Winfield Henderson <109438943+winhenderson@users.noreply.github.com>
2023-09-19 13:51:09 -0400

    exercise 5

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import Input from "../Input/Input";
 import Guesses from "../Guesses/Guesses";
+import { checkGuess } from "../../game-helpers";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -17,8 +18,33 @@ function Game() {
     <>
       <Guesses guesses={guesses} answer={answer} />
       <Input setGuesses={(guess) => setGuesses([...guesses, guess])} />
+      {guesses.filter((guess) => guess === answer).length > 0 ? (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in
+            <strong>
+              {guesses.filter((x) => x.length === 5).length > 0
+                ? guesses.filter((x) => x.length === 5).length === 1
+                  ? " 1 guess"
+                  : ` ${guesses.filter((x) => x.length === 5).length} guesses`
+                : ""}
+            </strong>
+            .
+          </p>
+        </div>
+      ) : (
+        guesses.length >= 6 && (
+          <div className="sad banner">
+            <p>
+              Sorry, the correct answer is <strong>{answer}</strong>.
+            </p>
+          </div>
+        )
+      )}
     </>
   );
 }
 
 export default Game;
+
+function checkIfWordIsCorrect(word, answer) {}
