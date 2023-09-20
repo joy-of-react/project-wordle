ada770fba2e402a3bb87036d572222ef983ebf21
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-09-20 12:39:37 -0700

    remove Keyboard from GuessResults component

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 import Guess from '../Guess';
-import Keyboard from '../KeyBoard';
 
 function GuessResults({ validatedGuesses }) {
-  const [keyboardStatus, setKeyboardStatus] = React.useState(
-    Array(26).fill(undefined)
-  );
-
   return (
     <>
       <div className="guess-results">
