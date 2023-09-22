bc6d6864807137f8196a4f643f4750854a021b22
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-09-22 12:34:31 -0700

    working thru exercise3

--- src/components/Guess/guess.js
+++ src/components/Guess/guess.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Slots from '../Slots/Slots';
 
 function Guess({inputs}){
     console.log("Inside Guesses Component", inputs);
     return(
     <>
     <div className="guess-results">
-        {inputs.map(({guess, id}) => (
-            <p key={id} className='guess'>{guess}</p>
+        {inputs.map(({guess, id}) => (  
+        //  <p key={id} className='guess'>{guess}</p>
+         <Slots guess={guess} id={id}/>
         ))}
-       
     </div>
     </>
     )
