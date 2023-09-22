0aeba0fa2b6947ce6a806869afbe25a1095224f0
robertjohanson93 <robert.johanson.93@gmail.com>
2023-09-22 18:36:35 +0200

    feat: exercise-1 added help text above input

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -12,7 +12,7 @@ function InputField() {
   return (
     <div>
       <form onSubmit={handleSubmit} className="guess-input-wrapper">
-        <label htmlFor="guess-input">Enter guess:</label>
+        <label htmlFor="guess-input">Enter guess (5 letters):</label>
         <input
           id="guess-input"
           type="text"
