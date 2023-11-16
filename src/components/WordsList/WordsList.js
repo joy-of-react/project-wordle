61725545c0257b21bf8bbbfcbaaf9f9658ec34cc
AlexRS90 <aramosbengy@gmail.com>
2023-11-15 21:08:25 -0600

    Create squares to display words

--- src/components/WordsList/WordsList.js
+++ src/components/WordsList/WordsList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Guess from '../Guess';
+import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
+import { range } from '../../utils';
 
 function WordsList({guesses}) {
   return(
     <div className="guess-results">
-      {guesses.map((item, index) => (
-        <p className="guess" key={index}>{item}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
