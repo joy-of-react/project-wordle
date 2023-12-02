ae1284fcc1d4a2ef8e3bf1b83f2454d18eed3ef2
alynch <anthonyvlynch5@gmail.com>
2023-12-01 19:36:06 -0500

    updated GuessResults to pass answer to Guess

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -4,12 +4,12 @@ import { range } from '../../utils';
 import Guess from '../Guess/Guess';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function GuessResults({ guessArray }) {
+function GuessResults({ guessArray, answer }) {
   
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map(num => {
-        return <Guess key={num} guess={guessArray[num]}/>
+        return <Guess key={num} guess={guessArray[num]} answer={answer}/>
       })}
     </div>
   );
