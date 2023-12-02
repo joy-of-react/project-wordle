7c9fe2b183cb3cf30d4d85c8bd6a88805a1548a8
alynch <anthonyvlynch5@gmail.com>
2023-12-01 23:41:18 -0500

    added status state to Game and modified handleAddGuess to set the status. Conditionally render win and loss banner instances

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,8 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import ImportForm from '../InputForm/InputForm';
 import GuessResults from '../GuessResults/GuessResults';
+import Banner from '../Banner/Banner';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,6 +14,7 @@ console.info({ answer });
 
 function Game() {
   const [guessArray, setGuessArray] = React.useState([]);
+  const [status, setStatus] = React.useState('in progress');
 
   const handleAddGuess = (guess) => {
     const newGuessArray = [
@@ -19,12 +22,19 @@ function Game() {
       guess
     ];
     setGuessArray(newGuessArray);
+    if (guess === answer) {
+      setStatus('win');
+    } else if (newGuessArray.length >= NUM_OF_GUESSES_ALLOWED) {
+      setStatus('lose');
+    }
   }
 
   return (
     <>
       <GuessResults guessArray={guessArray} answer={answer}/>
-      <ImportForm handleAddGuess={handleAddGuess}/>
+      <ImportForm handleAddGuess={handleAddGuess} status={status}/>
+      {status === "win" && <Banner variant='happy'>Win</Banner>}
+      {status === "lose" && <Banner variant='sad'>Lose</Banner>}
     </>
   );
 }
