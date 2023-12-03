e02a6bf5c6b7a34b4479493bd5427d1cf767c25e
alynch <anthonyvlynch5@gmail.com>
2023-12-03 03:15:52 -0500

    added handleRestart to LoseBanner and added button to call it onClick

--- src/components/WinBanner/WinBanner.js
+++ src/components/WinBanner/WinBanner.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import Banner from '../Banner/Banner';
 
-function WinBanner({ guessArray }) {
+function WinBanner({ guessArray, handleRestart }) {
   const numOfGuesses = guessArray.length === 1 ?
-    ' 1 guess' : `${ guessArray.length} guesses`;
+    '1 guess' : `${guessArray.length} guesses`;
   return (
     <Banner
       variant='happy'
     >
       <p>
         <strong>Congratulations!</strong> Got it in
-        <strong>{numOfGuesses}</strong>.
+        <strong>{' '}{numOfGuesses}</strong>.
       </p>
+      <button
+        onClick={handleRestart}
+      >
+        New Game
+      </button>
     </Banner>
   );
 }
