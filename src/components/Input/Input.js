0cedab18f4dbd6389c085f98252f2e43090d9ec3
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-12-29 16:33:57 -0500

    include validation in form

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Input = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.length !== 5) {
+      alert('The word must be equal to 5 letters');
+      return;
+    };
+
+    console.log({ inputValue });
+    setInputValue('');
+  };
+
   return (
-    <form className='guess-input-wrapper'>
-      <label for='guess-input'>Enter guess:</label>
+    <form
+      className='guess-input-wrapper'
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor='guess-input'>Enter guess:</label>
       <input
+        required
         id='guess-input'
         type='text'
+        value={inputValue}
+        onChange={event => {
+          setInputValue(event.target.value.toUpperCase())
+        }}
       />
     </form>
-  )
-}
+  );
+};
 
 export default Input;
