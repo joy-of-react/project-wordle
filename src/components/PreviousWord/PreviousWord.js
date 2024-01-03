fd0862b754a5693c307dce2e874d8ae8828b1824
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-01-03 11:48:34 -0500

    include answer prop to validate info

--- src/components/PreviousWord/PreviousWord.js
+++ src/components/PreviousWord/PreviousWord.js
@@ -3,13 +3,12 @@ import Guess from '../Guess/Guess';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
-function PreviousWord({ words }) {
-
+function PreviousWord({ words, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map(num => {
         return (
-          <Guess key={num} word={words[num]} />
+          <Guess key={num} word={words[num]?.value} answer={answer} />
         );
       })}
     </div>
