b909500bd45ea9a6e070619fc44de5abca289642
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-01-03 11:48:01 -0500

    include cell component with props to validate word

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { range } from '../../utils';
+import Cell from '../Cell/Cell';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ word }) {
+function Guess({ word, answer }) {
+  const result = checkGuess(word, answer);
+  console.log({ result });
+
   return (
     <p className="guess">
       {
         range(5).map(num => {
           return (
-            <span key={num} className="cell">
-              {word ? word.value[num] : undefined}
-            </span>
-          )
+            <Cell
+              key={num}
+              letter={result ? result[num].letter : undefined}
+              status={result ? result[num].status : undefined}
+            />
+          );
         })
       }
     </p>
