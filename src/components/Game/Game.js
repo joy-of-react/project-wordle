049d7b4b634e1469e007ceae8e29b803dda1d90f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-01-03 18:16:50 -0500

    include validation is game over or user win

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import Input from '../Input/Input';
 import PreviousWord from '../PreviousWord/PreviousWord';
+import Banner from '../Banner/Banner';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,15 +13,41 @@ console.info({ answer });
 
 function Game() {
   const [words, setWords] = useState([]);
+  const [isOver, setIsOver] = useState(false);
+  const [isWinner, setIsWinner] = useState(false);
 
   const handleSaveWord = (input) => {
     setWords([...words, input]);
+
+    if (answer === input.value) {
+      setIsWinner(true);
+      setIsOver(true);
+    } else if (words.length >= 5) {
+      setIsOver(true);
+      setIsWinner(false);
+    } else {
+      setIsOver(false);
+    }
   };
 
   return (
     <>
-      <PreviousWord words={words} answer={answer} />
-      <Input handleSaveWord={handleSaveWord} />
+      <PreviousWord
+        words={words}
+        answer={answer}
+      />
+      <Input
+        handleSaveWord={handleSaveWord}
+        isOver={isOver}
+      />
+      {isOver ?
+        <Banner
+          isWinner={isWinner}
+          answer={answer}
+          words={words}
+        /> :
+        undefined
+      }
     </>
   );
 }
