a2b9d090edb03cf32fb962f61a949751495dd725
Jonathan Shapiro <shapiroj18@gmail.com>
2024-02-27 17:32:52 -0500

    exercise 3

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,8 +2,10 @@ import React from "react";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
-import GuessResults from "../GuessResults/index.js";
 import GuessInput from "../GuessInput/index.js";
+import GuessResults from "../GuessResults/index.js";
+
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,9 +14,11 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
-  function handleAddGuess(word) {
-    newGuess = { id: crypto.randomUUID(), word: word };
-    setGuesses([...guesses, newGuess]);
+  function handleAddGuess(guess) {
+    if (guesses.length === NUM_OF_GUESSES_ALLOWED) {
+      return;
+    }
+    setGuesses([...guesses, guess]);
   }
 
   return (
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+import Guess from "../Guess/index.js";
+
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
+import { range } from "../../utils";
+
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map(({ id, word }) => {
-        return (
-          <p id={id} className="guess">
-            {word}
-          </p>
-        );
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => {
+        return <Guess key={num} word={guesses[num]} />;
       })}
     </div>
   );
