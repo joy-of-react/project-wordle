7017aaf44c11cee815ce6b1c0fbb01307c3d9f3e
Jonathan Shapiro <shapiroj18@gmail.com>
2024-02-27 00:47:15 -0500

    slight updates to exercise 1

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -8,15 +8,17 @@ function GuessInput() {
         className="guess-input-wrapper"
         onSubmit={(event) => {
           event.preventDefault();
-          console.log(input);
+          console.log({ input });
           setInput("");
         }}
       >
         <label htmlFor="guess-input">Enter guess:</label>
         <input
           id="guess-input"
+          required
           type="text"
           pattern="[a-zA-Z]{5}"
+          title="5 letter word"
           value={input}
           onChange={(event) => setInput(event.target.value.toUpperCase())}
         />
