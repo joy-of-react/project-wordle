aa6393c2e738c3b4bd48d374a8c1a9325beeb121
Alex Stocki <ladislao.stocki@gointegro.com>
2024-03-11 23:04:31 -0300

    fix: shows the board and completes it with user guesses

--- src/components/UserGuesses/UserGuesses.js
+++ src/components/UserGuesses/UserGuesses.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function UserGuesses({ guesses }) {
+  const guessesArray = range(NUM_OF_GUESSES_ALLOWED - 1).fill([]).map((_, index) => {
+    return guesses[index] || ['', '', '', '', ''];
+  });
+
+  const randonIndex = () => (Math.random());
+
+
   return (
     <>
       <h2>Your guesses:</h2>
       <div className='guess-results'>
-        {guesses.map((guess, index) => (
-          <p className='guess' key={index}>
-            {guess.split('').map((letter, letterIndex) => {
-              <span className='cell' key={letter}>
+        {guessesArray.map((guess) => (
+          <p className='guess' key={randonIndex()}>
+            {Array.from(guess).map((letter) => (
+              <span className='cell' key={randonIndex()}>
                 {letter}
               </span>
-              })}
+              ))}
           </p>
         ))}
       </div>
