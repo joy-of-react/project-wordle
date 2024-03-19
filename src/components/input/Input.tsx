4707d2a9c95733e0685c1fb22dc6f9ca69d5c810
jynxio <jinxiaomatrix@gmail.com>
2024-03-20 00:07:24 +0800

    feat: add auto-focus and shortcut focus

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Input({ enabled, guess }: { enabled: boolean; guess: (word: string) => void }) {
+    const input = React.useRef<HTMLInputElement>(null);
     const [word, setWord] = React.useState('');
+
+    React.useEffect(() => {
+        input.current?.focus();
+        globalThis.addEventListener('keydown', handleKeydown);
+
+        return () => globalThis.removeEventListener('keydown', handleKeydown);
+
+        function handleKeydown(event: KeyboardEvent) {
+            'qwertyuiopasdfghjklzxcvbnm'.includes(event.key.toLowerCase()) && input.current?.focus();
+        }
+    }, []);
 
     return (
         <form onSubmit={handleSubmit} className="rounded border-2 border-cust-2 px-3 py-2">
@@ -17,6 +29,7 @@ function Input({ enabled, guess }: { enabled: boolean; guess: (word: string) => 
                 disabled={!enabled}
                 onChange={handleChange}
                 className="w-80 font-mono text-lg text-cust-4 placeholder:ps-1 placeholder:font-mono placeholder:text-cust-2"
+                ref={input}
             />
         </form>
     );
