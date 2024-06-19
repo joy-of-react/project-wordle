2ca0b47abf252b9a1b40576d2ae4ec2301e567b6
Scarlett <hao.sijia04@gmail.com>
2024-06-19 19:24:33 -0400

    ex1, input

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-
+import GuessInput from '../GuessInput/GuessInput';
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return <>
+    <GuessInput />
+  </>;
 }
 
 export default Game;
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function GuessInput() {
-  return <div></div>;
+  const [input, setInput] = React.useState("");
+  function 
+  return (
+    <div>
+      <form className="guess-input-wrapper">
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          pattern="[a-zA-Z]{5}"
+          title="please enter 5 letters"
+          aria-label="guess word input"
+          value={input}
+          onChange={(event) => {
+            setInput((event.target.value).toUpperCase());
+          }}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default GuessInput;
