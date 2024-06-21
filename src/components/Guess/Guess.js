df993d154179b8065fc194df12cfc2ec388f653c
Scarlett <hao.sijia04@gmail.com>
2024-06-21 16:45:05 -0400

    ex3 done

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,25 +3,27 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 
 function Guess({ guesses }) {
-  const cell = guesses.map((guess) =>
-    guess.split("").map((charOfGuess, index) => (
-      <span class="cell" key={index}>
-        {charOfGuess}
-      </span>
-    ))
-  );
+  // const cell = guesses.map((guess) =>
+  //   guess.split("").map((charOfGuess, index) => (
+  //     <span class="cell" key={index}>
+  //       {charOfGuess}
+  //     </span>
+  //   ))
+  // );
 
   return (
     <div>
-      {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
-        <p className="guess" key={index}>
-          {guesses.map((guess) =>
-            guess.split("").map((charOfGuess, index) => (
-              <span class="cell" key={index}>
-                {charOfGuess}
-              </span>
-            ))
-          )}
+      {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => (
+        <p className="guess" key={rowIndex}>
+          {guesses[rowIndex]
+            ? guesses[rowIndex].split("").map((char, index) => (
+                <span className="cell" key={index}>
+                  {char}
+                </span>
+              ))
+            : range(5).map((cellIndex) => (
+                <span className="cell" key={cellIndex}></span>
+              ))}
         </p>
       ))}
     </div>
