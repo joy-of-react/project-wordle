0a9479af5787a1f3d5f66da31790d454c1a8a730
Scarlett <hao.sijia04@gmail.com>
2024-06-22 17:54:47 -0400

    ternary expression syntax bug fixed

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -18,16 +18,20 @@ function Guess({ value }) {
       </p> */}
       <p className="guess">
         {checkResult
-          ? checkResult.map(
-              ({ letter, status }) =>
-                status === "correct" &&
-                (<span className="cell correct">{letter}</span>)(
-                  status === "incorrect"
-                ) &&
-                (<span className="cell incorrect">{letter}</span>)(
-                  status === "misplaced "
-                ) && <span className="cell misplaced ">{letter}</span>
-            )
+          ? checkResult.map(({ letter, status }, index) => (
+              <span
+                className={`cell ${
+                  status === "correct"
+                    ? "correct"
+                    : status === "incorrect"
+                    ? "incorrect"
+                    : "misplaced"
+                }`}
+                key={index}
+              >
+                {letter}
+              </span>
+            ))
           : range(5).map((index) => <span className="cell" key={index}></span>)}
       </p>
     </>
