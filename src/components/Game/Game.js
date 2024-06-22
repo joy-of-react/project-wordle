597346d064c0d7b2b38c96ab4905e96b2cc50456
Scarlett <hao.sijia04@gmail.com>
2024-06-22 13:39:29 -0400

    checkResult is null. Type error

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,7 +7,7 @@ import GuessesList from "../GuessesList/GuessesList";
 import Guess from "../Guess/Guess";
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+export const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,31 +1,33 @@
 import React from "react";
-
 import { range } from "../../utils";
+import { answer } from "../Game/Game";
+import { checkGuess } from "../../game-helpers";
 
 function Guess({ value }) {
+  const checkResult = checkGuess(value, answer);
+
   return (
-    // <div>
-    //   {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => (
-    //     <p className="guess" key={rowIndex}>
-    //       {guesses[rowIndex]
-    //         ? guesses[rowIndex].split("").map((char, index) => (
-    //             <span className="cell" key={index}>
-    //               {char}
-    //             </span>
-    //           ))
-    //         : range(5).map((cellIndex) => (
-    //             <span className="cell" key={cellIndex}></span>
-    //           ))}
-    //     </p>
-    //   ))}
-    // </div>
-    <p className="guess">
-      {range(5).map((index) => (
-        <span className="cell" key={index}>
-          {value ? value[index] : undefined}
-        </span>
-      ))}
-    </p>
+    <>
+      <p className="guess">
+        {range(5).map((index) => (
+          <span className="cell" key={index}>
+            {value ? value[index] : undefined}
+          </span>
+        ))}
+      </p>
+      <p className="guess">
+        {checkResult.map(
+          ({ letter, status }) =>
+            status === "correct" &&
+            (<span className="cell correct">{letter}</span>)(
+              status === "incorrect"
+            ) &&
+            (<span className="cell incorrect">{letter}</span>)(
+              status === "misplaced "
+            ) && <span className="cell misplaced ">{letter}</span>
+        )}
+      </p>
+    </>
   );
 }
 
